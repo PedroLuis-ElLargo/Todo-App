9803574e2ab2aecd11d69ed2ee2892065aa8f206
Pedro-Valenzuela <pedroluisvp99@gmail.com>
2023-07-03 16:29:34 -0400

    Clase 04 completada

--- app.js
+++ app.js
@@ -1,31 +1,57 @@
+( () => {
 
-const btnAgregar = document.querySelector( '[data-form-btn]' );
+  const btnAgregar = document.querySelector('[data-form-btn]');
+  const inputTarea = document.querySelector('[data-form-input]');
+  const list = document.querySelector('[data-list]');
 
-const createTask = ( evento ) => {
+  const createTask = (evento) => {
     evento.preventDefault();
-    const inputTarea = document.querySelector( '[data-form-input]' );
-    const value = inputTarea.value;
+
+    const value = inputTarea.value.trim();
+
+    if (value === '') {
+      alert("No se puede agregar contenido Vacio");
+      return;
+    }
 
     const task = document.createElement( 'li' );
-    task.classList.add( 'card' )
-    const list = document.querySelector( '[data-list]' );
+    task.classList.add( 'card' );
 
-    if(inputTarea.value == ''){
-        alert( "No se puede agregar una tarea vacia" );
-        //return false;
-    }else{
-        const content = `
-        <div>
-            <i class="far fa-check-square icon"></i>
-            <span class="task">${ value }</span>
-        </div>
-        <i class="fas fa-trash-alt trashIcon icon"></i>
-    `
-    task.innerHTML = content;
-    list.appendChild( task ); 
+    const taskContent = document.createElement( 'div' );
+    taskContent.appendChild( checkComplete() );
+    
+    const titleTask = document.createElement( 'span' );
+    titleTask.classList.add( 'task' );
+    titleTask.innerText = value;
+    taskContent.appendChild( titleTask );
+
+    task.appendChild( taskContent );
+
+    const trashIcon = document.createElement( 'i' );
+    trashIcon.classList.add( 'fas', 'fa-trash-alt', 'trashIcon', 'icon' );
+    task.appendChild( trashIcon );
+
+    list.appendChild( task );
     inputTarea.value = '';
-    }   
-};
 
-//Arrow functions o funciones anonimas
-btnAgregar.addEventListener( 'click', createTask );
+    trashIcon.addEventListener( 'click', () => {
+      list.removeChild( task );
+    });
+  };
+
+  btnAgregar.addEventListener( 'click', createTask );
+
+  const checkComplete = () => {
+    const icon = document.createElement( 'i' );
+    icon.classList.add( 'far', 'fa-check-square', 'icon' );
+    icon.addEventListener( 'click', completeTask );
+    return icon;
+  };
+
+  const completeTask =  (event ) => {
+    const icon = event.target;
+    icon.classList.toggle( 'fas' );
+    icon.classList.toggle( 'completeIcon' );
+    icon.classList.toggle( 'far' );
+  };
+})();
